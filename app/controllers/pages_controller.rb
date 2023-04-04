2a7ec3b3f3162c79f4f2a7244e7486e01659d60b
ixyhoo <ixyhoo@outlook.fr>
2023-04-04 20:42:32 +0200

    finish the exercise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,9 @@ class PagesController < ApplicationController
 
     def contact
         @members = ["joe", "william", "max", "dylan", "bob"] 
+
+        if params[:member]
+            @members = @members.select { |member| member.start_with?(params[:member])}
+        end
     end
 end
